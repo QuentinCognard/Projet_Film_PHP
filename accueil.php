6d024c730561c2e3d86e52561ab9be101615f098
Quentin Cognard <q.cognard@hotmail.fr>
2017-10-06 12:48:56 +0200

    modification de la barre de recherche, implémentation d'une comboBox

--- accueil.php
+++ accueil.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <title>Page d'acceuil</title>
-     <link rel="stylesheet" type="text/css" href="accueil.css">
+    <link rel="stylesheet" type="text/css" href="accueil.css">
   </head>
   <header>
     <h1> Film Search</h1>
@@ -12,6 +12,27 @@
     <section>
       <h2> Recherchez un film</h2>
     <?php
+
+      $criteresCombo=array(
+        "type" => "comboBox",
+        "name" => "criteresCombo",
+        "value" => "criteresCombo",
+        "text" => "Rechercher par",
+        "text2" => ":",
+        "choices" =>[
+        array(
+          "text" => "Nom",
+          "value" => "nom"
+        ),
+        array(
+          "text" => "Pays",
+          "value" => "pays"
+        ),
+        array(
+          "text" => "Realisateur",
+          "value" => "realisateur"
+        )]
+      );
 
       $criteres =
         [array(
@@ -28,11 +49,29 @@
         ]
       )];
     ?>
+
     <form action='filmRecherche.php' method="POST">
-      Rechercher par nom : <input type="text" name="recherche">
-      <input type="submit" name="rechercheFilm">
         <?php
-        function question_radio($criteres){
+        function criteres_checkbox($criteresCombo){
+          $html = $criteresCombo["text"];
+          $html .= "<select name='$criteresCombo[name]'>";
+          foreach ( $criteresCombo["choices"] as $cc){
+            $html .= "<option value='$cc[value]'>$cc[text]</option>";
+          }
+          $html .= "</select>";
+          $html .= $criteresCombo["text2"];
+          echo $html;
+        }
+
+        criteres_checkbox($criteresCombo);
+
+        ?>
+      <input type="text" name="recherche">
+      <input type="submit" name="rechercheFilm">
+      <br>
+
+        <?php
+        function criteres_radio($criteres){
           $html = $criteres["text"] . "<br>";
           $i = 0;
           foreach ( $criteres["choices"] as $c){
@@ -44,16 +83,13 @@
         }
 
         foreach($criteres as $c){
-          question_radio($c);
+          criteres_radio($c);
         }
-
 
          ?>
          <br><input type="submit" name="criteresFilm">
 
     </form>
-
-
   </section>
   </body>
 </html>
